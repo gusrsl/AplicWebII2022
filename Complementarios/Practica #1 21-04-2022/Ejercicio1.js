950a09380f693c04aafdb801070f27aee29c86c1
gusrsl <89959859+gusrsl@users.noreply.github.com>
2022-04-22 19:23:19 -0500

    Subida de la carpeta de complementos
    Practica #1

---
+++ Complementarios/Practica #1 21-04-2022/Ejercicio1.js
@@ -0,0 +1,17 @@
+function escribirTablaMultiplicar(x){
+
+	console.log("Tabla de multiplicar del "+ x );
+
+    
+	//i++ significa i=i+1;
+
+	for(i = 1;i<=10;i++){
+		
+		console.log(x + "x " + i + "= " + x * i);
+		
+	}
+
+	
+}
+
+console.log(escribirTablaMultiplicar(128320));
